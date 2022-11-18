8265897a8e0479c2ad8aa8a9cdd36fb119075c4a
Mditran <micheldiaz@unicauca.edu.co>
2022-11-18 06:10:50 -0500

    Se agrego la preparacion antes de la vacunacion

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-$(window).resize(function () {
-  if (window.innerWidth > 860) {
-    $("#nav").removeClass("vertical");
-  }
-});
+const accordions = document.querySelectorAll(".accordion");
 
-$("#menu").click(function () {
-  $("#nav").toggleClass("vertical");
-  return false;
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 });
